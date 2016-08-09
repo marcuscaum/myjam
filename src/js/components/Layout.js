f4e12f8f7d1aadb7b036fdf2ef31f907fc2f7281
marcuscaum <marcuslucas.jb@gmail.com>
2016-08-09 20:14:42 -0300

    visual and component start

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,25 +1,16 @@
 import React from "react";
 
-import Footer from "./Footer";
-import Header from "./Header";
+import SongRequest from "./SongRequest";
 
 export default class Layout extends React.Component {
   constructor() {
     super();
-    this.state = {
-      title: "dasdqw",
-    };
-  }
-
-  changeTitle(title) {
-    this.setState({title});
   }
 
   render() {
     return (
       <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
-        <Footer />
+        <SongRequest/>
       </div>
     );
   }
---
+++ src/js/components/SongRequest.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class SongRequest extends React.Component {
+  requestSong(e) {
+
+  }
+
+  render() {
+    return (
+      <div id="song-request">
+        <input type="text" placeholder="What is your jam?"/>
+        <input type="button" value="xunda" />
+      </div>
+    );
+  }
+}
