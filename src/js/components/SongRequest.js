87b5aa052076f667290eadb7ab634d1f336cea38
marcuscaum <marcuslucas.jb@gmail.com>
2016-08-13 17:01:56 -0300

    create states and list

--- src/js/components/SongRequest.js
+++ src/js/components/SongRequest.js
@@ -1,15 +1,43 @@
 import React from "react";
+import SongList from "./SongList";
 
 export default class SongRequest extends React.Component {
-  requestSong(e) {
 
+  constructor() {
+    super();
+    this.state = {
+      songs: [],
+      videoURL: ''
+    };
+  }
+
+  onChange(e) {
+    this.setState({videoURL: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.videoURL) {
+      var nextSongs = this.state.songs.concat([{
+        video_url: this.state.videoURL
+      }]);
+      this.setState({
+        songs: nextSongs,
+        videoURL: ''
+      });
+    };
   }
 
   render() {
     return (
-      <div id="song-request">
-        <input type="text" placeholder="What is your jam?"/>
-        <input type="button" value="xunda" />
+      <div>
+        <div id="song-list">
+          <SongList items={this.state.songs} />
+        </div>
+        <form onSubmit={ this.handleSubmit.bind(this) } id="song-request">
+          <input type="text" placeholder="What is your jam?" onChange={ this.onChange.bind(this) } value={ this.state.videoURL }/>
+          <input type="submit" value="xunda" />
+        </form>
       </div>
     );
   }
